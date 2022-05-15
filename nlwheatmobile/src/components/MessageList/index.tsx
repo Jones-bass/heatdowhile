d91240a5df610747a0f2ed2f68f69fbfbd8fd100
Jones-bass <jonesbass.tb@gmail.com>
2022-05-15 20:15:38 -0300

    Estruturando as MessageList

---
+++ nlwheatmobile/src/components/MessageList/index.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { ScrollView, View } from 'react-native';
+import { Message } from '../Message';
+import { styles } from './styles';
+
+
+export function MessageList() {
+  const message = {
+    id: '1',
+    text: 'Testando Mensagem',
+    user: {
+      name: 'Jones Bass',
+      avatar_url: 'https://github.com/Jones-bass.png',
+    }
+  }
+
+   return (
+    <ScrollView style={styles.container}
+      contentContainerStyle={styles.content}
+      keyboardShouldPersistTaps="never"
+      >
+      <Message data={message}/>
+
+    </ScrollView>
+  );
+}
+
