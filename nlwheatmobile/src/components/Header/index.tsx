27616235696cc71a2e793486e877a45d7b032979
Jones-bass <jonesbass.tb@gmail.com>
2022-05-15 19:17:32 -0300

    Organizando Photo do perfil

--- nlwheatmobile/src/components/Header/index.tsx
+++ nlwheatmobile/src/components/Header/index.tsx
@@ -3,17 +3,22 @@ import { View, Text, TouchableOpacity } from 'react-native';
 
 import { styles } from './styles';
 import LogoSvg from '../../assets/logo.svg';
+import { UserPhoto } from '../UserPhoto';
 
 export function Header() {
   return (
     <View style={styles.container}>
       <LogoSvg />
 
+      <View style={styles.logoutButton}>
       <TouchableOpacity>
         <Text style={styles.logoutText}>
           Sair
         </Text>
       </TouchableOpacity>
+
+      <UserPhoto imageUri='https://github.com/Jones-bass.png' />
+    </View>
     </View>
   );
 }
